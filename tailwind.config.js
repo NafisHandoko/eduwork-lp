6176a99df04d8f0a5e9263063d04b463c43b780c
Nafis Handoko <naffisioner@gmail.com>
2023-09-24 00:31:11 +0700

    refactor(ui): added a lot of sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ tailwind.config = {
             colors: {
                 "eduwork": {
                     "bg": "#F6FCFF",
-                    "main": "#187CFF"
+                    "main": "#187CFF",
+                    "star": "#FFBD14",
+                    "text": {
+                        "base": "#404040",
+                        "heading": "#303030",
+                        "paragraph": "#676767",
+                        "nav": "#374151"
+                    }
                 }
             }
         }
