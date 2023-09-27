580baa7aa790046295c9cd3c6822d5579fc4e28d
Nafis Handoko <naffisioner@gmail.com>
2023-09-27 15:39:42 +0700

    refactor(ui): added some sections

--- eduwork-landing/tailwind.config.cjs
+++ eduwork-landing/tailwind.config.cjs
@@ -17,6 +17,28 @@ module.exports = {
       colors: {
         primary: "#187CFF",
         secondary: "#FFD200",
+        "eduwork": {
+          "bg": "#F6FCFF",
+          "main": "#187CFF",
+          "star": "#FFBD14",
+          "text": {
+            "base": "#404040",
+            "heading": "#303030",
+            "paragraph": "#676767",
+            "nav": "#374151"
+          },
+          "neutral": {
+            "10": "#FFFFFF",
+            "20": "#F5F5F5",
+            "30": "#EDEDED",
+            "40": "#E0E0E0",
+            "60": "#9E9E9E",
+            "70": "#757575",
+            "80": "#616161",
+            "90": "#404040",
+            "100": "#141414"
+          },
+        }
       },
     },
   },
