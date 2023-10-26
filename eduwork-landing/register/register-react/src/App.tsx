7fcee3c685b3004f67cc5da50c4a09445a59883d
Nafis Handoko <naffisioner@gmail.com>
2023-10-26 23:34:04 +0700

    refactor

--- eduwork-landing/register/register-react/src/App.tsx
+++ eduwork-landing/register/register-react/src/App.tsx
@@ -1,10 +1,19 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
+import { Route, Routes, Navigate } from 'react-router-dom'
+import Register from './pages/register'
+import Step1 from './pages/step1'
 
 function App() {
   return (
-    <></>
+    <>
+      <Routes>
+        <Route path='/' element={<Register />}>
+          <Route path='step1' element={<Step1 />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
 
