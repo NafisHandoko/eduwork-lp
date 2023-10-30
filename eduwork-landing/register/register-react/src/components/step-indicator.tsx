3112c3a622488ac08d7078bdd655d4e33efd975e
Nafis Handoko <naffisioner@gmail.com>
2023-10-30 13:46:48 +0700

    feat: indicator color change when all inputs are filled

--- eduwork-landing/register/register-react/src/components/step-indicator.tsx
+++ eduwork-landing/register/register-react/src/components/step-indicator.tsx
@@ -1,12 +1,27 @@
 import { NavLink } from "react-router-dom";
+import { useRegisterContext } from "../hooks/useRegisterContext";
+import { useState, useEffect } from 'react'
 
 export default function StepIndicator() {
+    const { state } = useRegisterContext()
+    const [step1Finished, setStep1Finished] = useState(false)
+
+    useEffect(() => {
+        if (state) {
+            if (state.username.length > 0 && state.email.length > 0 && state.password.length > 0 && state.confirmPassword.length > 0 && state.phoneNumber.length > 0 && state.sourceOfInfo != null) {
+                setStep1Finished(true)
+            } else {
+                setStep1Finished(false)
+            }
+        }
+    }, [state])
+
     return (
         <section className="container mx-auto px-3 xl:px-[135px] 2xl:px-[265px] hidden md:grid grid-cols-6 gap-1 mt-5 px-10">
             <NavLink
                 to='/step1'
                 className={({ isActive }) =>
-                    `flex flex-col border-t-8 gap-3 ${isActive ? 'border-primary' : 'border-eduwork-neutral-40'}`
+                    `flex flex-col border-t-8 gap-3 ${isActive ? 'border-primary' : step1Finished ? 'border-[#43936C]' : 'border-eduwork-neutral-40'}`
                 }
             >
                 <span className="text-eduwork-text-heading text-xl font-medium">STEP 1</span>
